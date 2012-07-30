fb07259946b589c1d4f5e5d429454a46ac7890b9
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2012-07-30 18:57:06 +0200

    add callback

--- jack_client.h
+++ jack_client.h
@@ -8,22 +8,39 @@
 using std::string;
 using std::runtime_error;
 
+extern "C" {
+	int jack_process(jack_nframes_t, void *arg);
+}
+
+namespace seqpp {
+
 struct jack_client {
 	jack_client_t *client;
 
 	jack_client(const string name) {
 		jack_status_t jack_status;
+
 		client = jack_client_open(name.c_str(), JackNullOption, &jack_status);
+
 		if (0 == client) {
 			throw runtime_error("failed to open jack client");
 		}
+
+		jack_set_process_callback(client, jack_process, (void*)this);
+
+		jack_activate(client);
 	}
 
+	int process(jack_nframes_t nframes) {
+		return 0;
+	}
 };
 
+} // namespace
+
 extern "C" {
-	int jack_process(jack_nframes_t, void *arg) {
-		return 0;
+	int jack_process(jack_nframes_t nframes, void *arg) {
+		return ((seqpp::jack_client*)arg)->process(nframes);
 	}
 }
 
