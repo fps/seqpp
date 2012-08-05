fdece0eb1ce82e3f53570b4111ff8ff3ebde7183
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2012-08-05 14:07:21 +0200

    bugs

--- test_jack_midi_consumer.lua
+++ test_jack_midi_consumer.lua
@@ -1,11 +1,9 @@
 require "seqpp"
 
--- h = seqpp.heap:get_instance()
-
 jmc = seqpp.jack_midi_consumer("test")
 
 time = jmc:time()
 
-for i = 1, 2000 do
+for i = 1, 10 do
 	jmc:post_event(time + i * 48000, seqpp.midi_event(), true)
 end
