d084ac493cb992e43bba7b6ad177f2d0358c45a3
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2012-08-05 14:44:58 +0200

    yay

--- seqpp/ringbuffer.h
+++ seqpp/ringbuffer.h
@@ -27,6 +27,7 @@ struct ringbuffer {
 
 	ringbuffer(unsigned int size = 1024) : size(size) {
 		jack_ringbuffer = jack_ringbuffer_create(sizeof(T) * size);
+		std::cout << "write space: " << jack_ringbuffer_write_space(jack_ringbuffer) << std::endl;
 
 		for (unsigned int i = 0; i < size; ++i) {
 			T *t = new (jack_ringbuffer->buf + sizeof(T) * i) T();
@@ -42,8 +43,8 @@ struct ringbuffer {
 	}
 
 	bool empty() {
-		std::cout << "space: " << jack_ringbuffer_write_space(jack_ringbuffer) <<  " sizeof(T): " << sizeof(T) << " size: " << size << std::endl;
-		return jack_ringbuffer_write_space(jack_ringbuffer) == sizeof(T) * size;
+		// std::cout << "space: " << jack_ringbuffer_write_space(jack_ringbuffer) <<  " sizeof(T): " << sizeof(T) << " size: " << size << std::endl;
+		return jack_ringbuffer_write_space(jack_ringbuffer) >= sizeof(T) * (size - 1);
 	}
 
 	bool can_write() {
@@ -77,8 +78,17 @@ struct ringbuffer {
                 return ((T*)rb_data->buf);
 	}
 
+	/*
+		read a T from the ringbuffer and replace it with a default constructed one.
+		NOTE: this is only suitable for shared_ptr or similar
+	*/
 	T read() {
-		T t = *peek();
+		T *tp = peek();
+
+		T t = *tp;
+
+		*tp = T();
+		
 		jack_ringbuffer_read_advance(jack_ringbuffer, sizeof(T));
 		return t;
 	}
